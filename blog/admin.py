b7ce4618733d88e49af0b76a2694a925395952b5
lijinghua <lijinghua@memect.co>
2018-08-29 10:48:20 +0800

    博客界面能看到了

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+#将BlogArticles引用到当前环境
+from .models import BlogArticles
+#注册到admin
+admin.site.register(BlogArticles)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+#django 2.0 创建外键时需要在后面加上on_delete
+
+class BlogArticles(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User,related_name="blog_posts",on_delete=models.CASCADE,)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+
+    #倒序显示
+    class Meta:
+        ordering = ("-publish",)
+    def __str__(self):
+        return self.title
+
+
+
